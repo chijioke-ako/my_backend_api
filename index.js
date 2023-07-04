c87899ee0f2a2a6fa14ea6ecea72fd1b808d059f
Chijioke Nwadimkpa <cnwadimkpa@teqbridgeltd.com>
2023-07-04 22:48:16 +0100

    deployment prosess

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send('Express app');
+});
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
