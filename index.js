c77e393d662b160101e6b298d4d7e73c5de7ab44
Chijioke Nwadimkpa <cnwadimkpa@teqbridgeltd.com>
2023-07-04 22:57:19 +0100

    deployment prosess add dotenv

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
