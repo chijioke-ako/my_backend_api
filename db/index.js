d0d157d0fb65ddf51944e592955dfb887b423fb9
Chijioke Nwadimkpa <cnwadimkpa@teqbridgeltd.com>
2023-07-05 14:10:13 +0100

    deployment prosess

--- db/index.js
+++ db/index.js
@@ -15,17 +15,19 @@
 
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT,
+//   max: 20,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 2000,
+// });
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionString: process.env.POSTGRES_URLB + '?sslmode=require',
 });
-
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
