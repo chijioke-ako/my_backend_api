fb8849644b20188f6d57ab87a6789d4041db8cb6
Chijioke Nwadimkpa <cnwadimkpa@teqbridgeltd.com>
2023-07-17 21:33:01 +0100

    Updated index.html with a new line

--- db/index.js
+++ db/index.js
@@ -12,10 +12,7 @@ const { Pool } = require('pg');
 // });
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: process.env.POSTGRES_URLB + '?sslmode=require',
 });
 
 module.exports = pool;
